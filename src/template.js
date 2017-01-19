2a16db95611b6631485b8ccea9fe41cb7284bb6c
manhhailua <manhhailua@gmail.com>
2017-01-19 14:56:01 +0700

    simplify template

--- src/template.js
+++ src/template.js
@@ -4,58 +4,23 @@
 
 /* global Arf */
 
-// Check env
-let env = '';
+const env = (location.search.indexOf('corejs_env=dev') !== -1) ? '' : '.min';
+const script = document.createElement('script');
 
-switch (true) {
-  // Development mode
-  case location.search.indexOf('corejs_env=dev') !== -1: {
-    env = '';
-    break;
-  }
-  // Pre-build mode (vue development tool included)
-  case location.search.indexOf('corejs_env=pre') !== -1: {
-    env = '.build';
-    break;
-  }
-  // Production mode
-  default: {
-    env = '.min';
-  }
-}
+script.id = 'arf-core-js';
+script.type = 'text/javascript';
+script.src = `//corejs.manhhailua.com/build/Arf${env}.js`;
 
 /**
  * Async load core-js script
- * @param src
- * @param callback
  */
-function loadScript(src, callback) {
-  const script = document.createElement('script');
-  const node = document.getElementsByTagName('body')[0];
-
-  script.id = 'admicro-arf';
-  script.type = 'text/javascript';
-  script.src = src;
-  script.onload = script.onreadystatechange = callback;
-
-  if (!document.getElementById(script.id)) {
-    node.appendChild(script);
-  } else {
-    callback();
-  }
+if (!document.getElementById(script.id)) {
+  document.getElementsByTagName('body')[0].appendChild(script);
 }
 
-/**
- * Handle load script callback
- */
-function handle() {
-  // Init arfZonesQueue if not existed
-  window.arfZonesQueue = window.arfZonesQueue || [];
+// Init arfZonesQueue if not existed
+window.arfZonesQueue = window.arfZonesQueue || [];
 
-  // Push current zone to arfZonesQueue
-  // In production mode, webpack will force double quotes for string
-  window.arfZonesQueue.push("{{zoneDataObject}}"); // eslint-disable-line quotes
-}
-
-// Start load script
-loadScript(`//corejs.manhhailua.com/build/Arf${env}.js`, handle);
+// Push current zone to arfZonesQueue
+// In production mode, webpack will force double quotes for string
+window.arfZonesQueue.push("{{zoneDataObject}}"); // eslint-disable-line quotes
